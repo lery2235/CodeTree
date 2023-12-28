c880f499b7966abe5e36437686f578c29443ce35
lery2235 <50572117+lery2235@users.noreply.github.com>
2023-12-28 19:14:37 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231228/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
+++ 231228/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -7,13 +7,13 @@ int main() {
     cin >> a >> b;
 
     if(a < b){
-        cout << "1" << " ";
+        cout << 1 << " ";
     }
     else 
-        cout << "0";
+        cout << 0;
 
     if(a == b){
-        cout << "1" << " ";
+        cout << 1 << " ";
     }
     else 
         cout << 0;
