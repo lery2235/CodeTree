a19d3fb41480f0865dcb17c4dfc50daaffdb602a
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-01-01 22:14:09 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240101/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -4,13 +4,13 @@ int main() {
     int a_math, a_english;
     int b_math, b_english;
     
-    std::cin >> a_math >> a_english >> b_math >> b_englsih;
+    std::cin >> a_math >> a_english >> b_math >> b_english;
 
     if(a_math > b_math || (a_math == b_math && a_english > b_english)){
         std::cout << "A"; 
     }
     else
-        cout << "B";
+        std::cout << "B";
 
     return 0;
 }
