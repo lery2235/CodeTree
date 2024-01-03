06c80708f66b895fed0adc594d6ce509bf5e3397
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-01-03 17:21:54 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/두 사람/two-person.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a_age, b_age;
+    string a_sex, b_sex;
+
+    cin >> a_age >> a_sex;
+    cin >> b_age >> b_sex;
+    
+    if(a_age >= 19 && a_sex == "M" || b_age >= 19 && b_sex == "M"){
+        cout << 1;
+    }
+    else 
+        cout << 0;
+    
+    return 0;
+}
