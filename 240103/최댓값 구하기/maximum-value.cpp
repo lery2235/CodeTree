ec1fec660447e1db0f3ac79236ac0b3d0059edb6
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-01-03 18:38:31 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240103/최댓값 구하기/maximum-value.cpp
+++ 240103/최댓값 구하기/maximum-value.cpp
@@ -5,16 +5,15 @@ int main() {
     int a,b,c;
     cin >> a >> b >> c;
 
-    if(a > b && a != b){
-        if(a > c && a != c){
-            cout << a;
-        }
+    if(a > b && a != b && a > c && a != c){
+            cout << a; 
     }
-    else if(b > c && b != c){
+    if(b > c && b != c && b > a && b != a){
         cout << b;
     }
-    if(c > a && c > b && c != a && c != b)
+    if(c > a && c > b && c != a && c != b){
         cout << c;
+        }
 
 
     return 0;
