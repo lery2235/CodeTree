13a2019b2d7c7859491ab99bcf9817994d9f07d9
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-01-03 18:17:49 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240103/일 수 구하기/number-of-days-in-month.cpp
+++ 240103/일 수 구하기/number-of-days-in-month.cpp
@@ -12,13 +12,17 @@ int main() {
         else
             cout << 31;
     }
-    if(n <= 7){
+    if(n == 2){
+        cout << 28;
+    }
+    else if(n <= 7){
         if(n % 2 == 0){
             cout << 30;
         }
         else 
             cout << 31;
     }
+
 
 
     return 0;
