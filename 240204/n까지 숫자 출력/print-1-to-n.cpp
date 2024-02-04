1e6b01bec95c1a5abfbaf058a37ba2b727a25831
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-02-04 17:57:19 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240204/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    int i = 1;
+    
+    while(n >= i){
+        std::cout << i << " ";
+        i += 1;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
