80899ad4c8bec9fce9cbdb5705fb78d2c0d20b8d
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-02-05 13:18:07 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240205/높은 수에서 낮은 수까지/from-high-to-low.cpp
+++ 240205/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -4,9 +4,15 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    for(int i = b; i >= a; i--){
-        cout << i << " ";
+    if (a > b) {
+        for (int i = a; i >= b; i--) {
+            cout << i << " ";
+        }
     }
-    
+    else if (b > a) {
+        for (int i = b; i >= a; i--) {
+            cout << i << " ";
+        }
+    }
     return 0;
 }
