632ab2b6bc293b98e8c777fff4c6988fbf169c85
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-02-06 16:26:17 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/홀수만 출력하기/print-only-odd-numbers.cpp
+++ 240206/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int testcase;
     cin >> testcase;
-    for(int i = testcase; i >= 0; i--){
+    for(int i = testcase; i >= 1; i--){
         int n;
         cin >> n;
         if(n % 3 == 0 && n % 2 == 1){
