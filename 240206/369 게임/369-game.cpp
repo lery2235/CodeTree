462afdb49d4c3d92df5f089d2580bc928b3cdbfd
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-02-06 16:21:18 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/369 게임/369-game.cpp
+++ 240206/369 게임/369-game.cpp
@@ -3,13 +3,21 @@ using namespace std;
 
 int main() {
     int n;
+    int digit;
+    int digit_double;
     cin >> n;
-    for(int i = 1; i <= n; i++){
-        if(i % 3 == 0 || i == 3 || i == 6 || i == 9){
-            cout << 0 << " ";
+
+    for (int i = 1; i <= n; i++) {
+        digit = i % 10;
+        digit_double = i / 10;
+
+        if (digit_double == 3 || digit_double == 6 || digit_double == 9 || i % 3 == 0 || digit == 3 || digit == 6 || digit == 9) {
+            cout << "0" << " ";
         }
-        else
-            cout << i << " "; 
+        else {
+            cout << i << " ";
+        }
     }
+    
     return 0;
 }
