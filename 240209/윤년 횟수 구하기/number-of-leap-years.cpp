3f84aba38c8a7b9adc744b3d7ff39154b9e469c3
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-02-09 16:22:24 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240209/윤년 횟수 구하기/number-of-leap-years.cpp
+++ 240209/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
+    // 변수 선언 및 입력
     int n;
     cin >> n;
-    int cnt = 0;
-
-    for(int i = 1; i < n; i++){
-        if((i % 4 == 0 && i % 100 != 0) || i % 400 == 0){
-    
-            cnt++;
-        
-    }
-    }
-        cout << cnt;
-    return 0;
-    }
+	int cnt = 0;
+	
+	// 1년부터 n년까지 윤년의 횟수를 구합니다.
+	for(int i = 1; i <= n; i++) {
+		if((i % 4 == 0 && i % 100 != 0) || i % 400 == 0)
+			cnt++;
+	}
+		
+	cout << cnt;
+	
+	return 0;
+}
