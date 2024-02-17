b1c2f0631c0e5c2a097324c4c9df607c86c73b67
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-02-17 16:01:04 +0900

    Title: 홀수만 더하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240217/홀수만 더하기/add-only-odd-numbers.cpp
+++ 240217/홀수만 더하기/add-only-odd-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     int sum = 0;
 
-    for(int i = 0; i <= 100; i++){
+    for(int i = 0; i <= 10; i++){
         cin >> n;
         if(n % 3 == 0 && n % 2 == 1){
             sum += n;
