9ad1cc92c1fa696d56f39b2f95551911e44cf9f0
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-03-18 18:45:17 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240318/요일 맞추기/guess-day-of-week.cpp
+++ 240318/요일 맞추기/guess-day-of-week.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int DateToDay(int month, int day) {
     
-     int days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 31, 30, 31, 30};
+    int day_month[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 31, 30, 31, 30 };
+   
     //현재 월 까지 일 수 더하기 계산 
     int res = 0;
     for (int i = 1; i < month; i++) {
@@ -19,19 +20,21 @@ int DateToDay(int month, int day) {
 
 
 int main() {
-    int m1, m2, d1, d2;
+    int month, day, target_month, target_day;
 
-    cin >> m1 >> d1 >> m2 >> d2;
+    string v[] = {"Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"};
 
-    int diff = DateToDay(m2, d2) - DateToDay(m1, d1);
+    cin >> month >> day >> target_month>> target_day;
+
+    int day_cal = DateToDay(target_month, target_day) - DateToDay(month, day);
 
     // 주어진 날짜 사이의 요일 차이를 요일로 변환
-    while (diff < 0) {
-        diff += 7;
+    while (day_cal < 0) {
+        day_cal += 7;
     }
 
-    string day_of_week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-     cout << day_of_week[diff % 7];
+    // 요일로 변환하여 출력
+    cout << v[day_cal % 7];
 
     return 0;
 }
