c6324d90e86f801f84ed224cda90ac569d7269c6
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-03-18 18:37:59 +0900

    Title: 요일 맞추기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240318/요일 맞추기/guess-day-of-week.cpp
+++ 240318/요일 맞추기/guess-day-of-week.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
-int DateToDay(int month, int day, int target_month, int target_day) {
-    int day_month[] = { 0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+int DateToDay(int month, int day) {
+    
+    int day_month[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+   
+    //현재 월 까지 일 수 더하기 계산 
     int res = 0;
-
-    // 현재 월의 총 일 수 계산
-    int total_days = 0;
     for (int i = 1; i < month; i++) {
-        total_days += day_month[i];
+        res += day_month[i];
     }
-    total_days += day;
-
-    // 대상 월의 총 일 수 계산
-    int target_total_days = 0;
-    for (int i = 1; i < target_month; i++) {
-        target_total_days += day_month[i];
-    }
-    target_total_days += target_day;
-
-    // 대상 월의 총 일 수에서 현재 월의 총 일 수를 뺌
-    res = target_total_days - total_days;
-
+    // 일을 분으로 변경
+    res += day;
+    
     return res;
 }
+
 
 int main() {
     int month, day, target_month, target_day;
 
-    vector<string> v{"Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"};
+    string v[] = {"Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"};
 
-    cin >> month >> day >> target_month >> target_day;
+    cin >> month >> day >> target_month>> target_day;
 
-    int day_cal = DateToDay(month, day, target_month, target_day);
+    int day_cal = DateToDay(target_month, target_day) - DateToDay(month, day);
 
     // 주어진 날짜 사이의 요일 차이를 요일로 변환
     while (day_cal < 0) {
