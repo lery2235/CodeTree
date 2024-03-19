796b4182063b6b045f308512bd8afe12b9d20072
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-03-19 18:47:53 +0900

    Title: 공약수의 유무, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 240319/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> a >> b;
 
     for(int i = a; i <= b; i++){
-        if( 1920 % i && 2880 % i){
+        if(1920 % i == 0 && 2880 % i == 0){
             flag = true;
         }        
     }
