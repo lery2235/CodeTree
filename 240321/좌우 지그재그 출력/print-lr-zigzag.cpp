fce3cfd7cc4d14f2b6f0b34b9c4ef86fda303308
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-03-21 16:23:15 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/좌우 지그재그 출력/print-lr-zigzag.cpp
+++ 240321/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
     int res = 0;
+    int temp = 0;
     int n;
     cin >> n;
     for (int i = 0; i < n; i++) {
@@ -10,15 +11,15 @@ int main() {
             for (int j = 1; j <= n; j++) {
                 cout << res + j << " ";
                 if (j == n) {
-                    res = j + n + 1;
+                    res = (res + j) + n;
                 }
             }
         }
         else {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 0; j < n; j++) {
                 cout << res - j << " ";
-                if (j == n) {
-                    res = j + n;
+                if (j == n - 1) {
+                    res = (res - j) + n + 1 - 2;
                 }
             }
         }
