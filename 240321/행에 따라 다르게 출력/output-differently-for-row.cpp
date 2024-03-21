ee693e01acfb675e72bbb9a4e9d83ef70b3bc0bc
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-03-21 17:12:24 +0900

    Title: 행에 따라 다르게 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/행에 따라 다르게 출력/output-differently-for-row.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int res = 0;
+    int temp = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 1; j <= n; j++) {
+                cout << res + j << " ";
+                if (n == j) {
+                    res = res + j + 2;
+                }
+            }
+        }
+        else { // 1 * 2 = 2, 2 * 2 = 4
+            for (int j = 0; j < n; j++){ 
+                cout << res + (j * 2) << " ";
+                if (n - 1 == j) {
+                    res = res + j + n - i;
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
