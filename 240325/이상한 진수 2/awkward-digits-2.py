463d70ce81a2e937e8890881ba67c9e9f4b8c4d0
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-03-25 18:21:35 +0900

    Title: 이상한 진수 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/이상한 진수 2/awkward-digits-2.py
+++ 240325/이상한 진수 2/awkward-digits-2.py
@@ -1,17 +1,17 @@
 import sys 
 
-res = 0
-binary_number = sys.stdin.readline().strip()
-binary_number = [int(char) for char in binary_number]
+INT_MIN = -sys.maxsize
 
-for i in range(len(binary_number)):
-    if binary_number[i] != 1:
-        binary_number[i] = 1
-        break
-    else:
-        continue
+binary = list(map(int, list(input())))
+ans = INT_MIN
 
-for i in binary_number:
-    res = res * 2 + i
+for i in range(len(binary)):
+    binary[i] = 1 - binary[i]
 
-print(res)
+    num = 0
+    for j in range(len(binary)):
+        num = num * 2 + binary[j]
+    ans = max(ans, num)
+    binary[i] = 1 - binary[i]
+
+print(ans)
