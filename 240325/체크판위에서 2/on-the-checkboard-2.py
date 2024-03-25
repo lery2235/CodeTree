8624813e38828e96a5dc4920bef01c54d9da8cd0
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-03-25 15:54:43 +0900

    Title: 체크판위에서 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240325/체크판위에서 2/on-the-checkboard-2.py
+++ 240325/체크판위에서 2/on-the-checkboard-2.py
@@ -4,8 +4,8 @@ r, c = map(int, input().split())
 count = 0
 rect = [list(map(str, input().split()))for _ in range(r)]
 
-for i in range(r):
-    for j in range(c - 1):
+for i in range(1, r):
+    for j in range(1, c):
         for k in range(i + 1, r - 1):
             for l in range(j + 1, c - 1):
                 if(rect[i][j] != rect[0][0] and 
