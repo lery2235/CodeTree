04e850d8f4b96cb63b5f9394972cd6fd3517816a
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-04-02 19:42:03 +0900

    Title: 배열 채우기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240402/배열 채우기/filling-array.py
+++ 240402/배열 채우기/filling-array.py
@@ -3,6 +3,8 @@ res = [0] * len(arr)
 for i in range(len(arr)):
     if arr[i] == 0:
         res[:i+1] = reversed(arr[:i])
+    else:
+        res[0:i+1] = reversed(arr[0:i+1])
 
 for i in res:
     print(i, end =' ')
