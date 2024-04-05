1bcd926a09a4748c348e43c7d2e7f165160ca2fc
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-04-06 03:03:56 +0900

    Title: 격자 위의 편안한 상태, Time: 227ms, Memory: 27MB, Status: Passed - CodeTree

--- 240406/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240406/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,6 +1,6 @@
 n, m = tuple(map(int, input().split()))
 
-dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 board = [
     [0] * n 
     for _ in range(n)
@@ -9,23 +9,22 @@ board = [
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-def check_color(r, c):
+def adjacent_cnt(x, y):
     cnt = 0
-    for i in range(4):
-        nx = r + dx[i]
-        ny = c + dy[i]
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
         if in_range(nx, ny) and board[nx][ny] == 1:
             cnt += 1
-
-        return cnt
+    
+    return cnt
 
 for i in range(m):
-    r, c = tuple(map(int, input().split()))
-    r -= 1
-    c -= 1
-    board[r][c] = 1
-    
-    if check_color(r, c) == 3:
+    x, y = tuple(map(int, input().split()))
+    x -= 1
+    y -= 1
+    board[x][y] = 1
+
+    if adjacent_cnt(x, y) == 3:
         print(1)
     else:
         print(0)
