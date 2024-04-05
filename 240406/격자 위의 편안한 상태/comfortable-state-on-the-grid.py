b78485358cd7cd66df15a82931db19a08c586f1b
lery2235 <50572117+lery2235@users.noreply.github.com>
2024-04-06 02:20:19 +0900

    Title: 격자 위의 편안한 상태, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240406/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240406/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -4,8 +4,14 @@ dx, dy = [0, 0, -1, 1], [1, -1, 0, 0]
 board = [[0] * (n + 1) for _ in range(n + 1)]
 
 def check_color(r, c):
+    if board[r-1][c-1] != 1:
+        board[r-1][c-1] = 1
+    else:
+        return 0
+
     cnt = 0
     for i in range(4):
+        
         nx = r + dx[i]
         ny = c + dy[i]
         if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 1:
@@ -17,5 +23,4 @@ def check_color(r, c):
 
 for i in range(m):
     r, c = map(int, input().split())
-    board[r-1][c-1] = 1
     print(check_color(r-1, c-1))
